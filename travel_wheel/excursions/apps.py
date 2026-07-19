4fda5cec89d439c559e4008384c5c9e9a5de48e9
Ralina <seitovaralina1@gmail.com>
2026-07-19 17:40:29 +0500

    refactor: delete unused travel_wheel folder

--- travel_wheel/excursions/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class ExcursionsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'excursions'
-    verbose_name = "Экскурсии"
